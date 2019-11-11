49ba9ae82308d3d41f61a068da1c96b5f05aa82a
bigman212 <bigman212@yandex.ru>
2019-11-11 17:53:22 +1000

    (design+refactor) refactor categories adapter and fix style

--- app/src/main/java/ru/bill/renote/notes/list/CategoriesListRVAdapter.kt
+++ app/src/main/java/ru/bill/renote/notes/list/CategoriesListRVAdapter.kt
@@ -1,36 +1,32 @@
 package ru.bill.renote.notes.list
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.rv_categories_item.view.*
 import ru.bill.renote.R
 
-class CategoriesListRVAdapter(private val strings: List<String>) :
-  androidx.recyclerview.widget.RecyclerView.Adapter<CategoriesListRVAdapter.ViewHolder>() {
+class CategoriesListRVAdapter(private val categoriesList: List<String>)
+  : RecyclerView.Adapter<CategoriesListRVAdapter.ViewHolder>()
+{
   override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
     val inflater = LayoutInflater.from(parent.context)
-    return ViewHolder(inflater, parent)
+    val inflatedView = inflater.inflate(R.layout.rv_categories_item, parent, false)
+    return ViewHolder(inflatedView)
   }
 
   override fun getItemCount(): Int {
-    return strings.size
+    return categoriesList.size
   }
 
-  override fun onBindViewHolder(p0: ViewHolder, p1: Int) {
-    p0.bind(strings[p1])
+  override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    holder.bind(categoriesList[position])
   }
 
-  class ViewHolder(inflater: LayoutInflater, parent: ViewGroup) :
-    androidx.recyclerview.widget.RecyclerView.ViewHolder(
-      inflater.inflate(
-        R.layout.recycler_view_categories_item,
-        parent,
-        false
-      )
-    ) {
-    fun bind(str: String) {
-      itemView.findViewById<Button>(R.id.tv_name).text = str
+  class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    fun bind(categoryTitle: String) {
+      itemView.tv_name.text = categoryTitle
     }
   }
-
 }
