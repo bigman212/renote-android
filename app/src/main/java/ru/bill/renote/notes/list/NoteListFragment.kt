2f313c9db93ed84efffba79dfd3904cb0e8b8e86
bigman212 <bigman212@yandex.ru>
2019-11-21 13:11:26 +1000

    (feature) show notes list from db

--- app/src/main/java/ru/bill/renote/notes/list/NoteListFragment.kt
+++ app/src/main/java/ru/bill/renote/notes/list/NoteListFragment.kt
@@ -2,33 +2,30 @@ package ru.bill.renote.notes.list
 
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.annotation.WorkerThread
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.findNavController
-import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.fragment_note_list.*
 import ru.bill.renote.R
 import ru.bill.renote.model.Resource
 
 class NoteListFragment : Fragment() {
   private lateinit var viewModel: NotesViewModel
-  private lateinit var rvAdapter: CategoriesListRVAdapter
+  private lateinit var rvCategoriesAdapter: CategoriesListRVAdapter
+  private lateinit var rvNotesAdapter: NotesListRVAdapter
 
   companion object {
     @JvmStatic
     fun newInstance() =
-      NoteListFragment().apply {
-        //                arguments = Bundle().apply {
-//                    putString(ARG_PARAM1, param1)
-//                    putString(ARG_PARAM2, param2)
-//                }
-      }
+      NoteListFragment().apply {}
+  }
+
+  override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    return inflater.inflate(R.layout.fragment_note_list, container, false)
   }
 
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -41,13 +38,24 @@ class NoteListFragment : Fragment() {
       it.findNavController().navigate(actionNoteListFragmentToNoteCreateFragment)
     }
 
-    rvAdapter = CategoriesListRVAdapter(mutableListOf())
-    rv_categories.adapter = rvAdapter
+    rvCategoriesAdapter = CategoriesListRVAdapter(mutableListOf())
+    rv_categories.adapter = rvCategoriesAdapter
+
+    rvNotesAdapter = NotesListRVAdapter(mutableListOf())
+    rv_notes.adapter = rvNotesAdapter
 
     viewModel.allCategories().observe(this, Observer { resource ->
-      when (resource){
+      when (resource) {
         is Resource.Success -> {
-          rvAdapter.addAll(resource.data!!)
+          rvCategoriesAdapter.addAll(resource.data!!)
+        }
+      }
+    })
+
+    viewModel.allNotes().observe(this, Observer { res ->
+      when (res) {
+        is Resource.Success -> {
+          rvNotesAdapter.addAll(res.data!!)
         }
       }
     })
@@ -56,13 +64,5 @@ class NoteListFragment : Fragment() {
 
   override fun onResume() {
     super.onResume()
-  }
-
-  override fun onCreateView(
-    inflater: LayoutInflater,
-    container: ViewGroup?,
-    savedInstanceState: Bundle?
-  ): View? {
-    return inflater.inflate(R.layout.fragment_note_list, container, false)
   }
 }
