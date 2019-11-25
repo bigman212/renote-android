6242e68d0fa6a1c20e4fe5936ae7a82f3bd8f83f
bigman212 <bigman212@yandex.ru>
2019-11-25 12:01:38 +1000

    (feature) show notes depending on categories selected

--- app/src/main/java/ru/bill/renote/notes/list/NotesViewModel.kt
+++ app/src/main/java/ru/bill/renote/notes/list/NotesViewModel.kt
@@ -3,7 +3,9 @@ package ru.bill.renote.notes.list
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import io.reactivex.Observable
 import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.subjects.PublishSubject
 import ru.bill.renote.App
 import ru.bill.renote.extensions.uiObserve
 import ru.bill.renote.model.Resource
@@ -13,19 +15,31 @@ import ru.bill.renote.model.entities.Note
 class NotesViewModel : ViewModel() {
   private val notesDao = App.db.notesDao()
   private val categoriesDao = App.db.categoriesDao()
+  private val noteCategoryDao = App.db.noteCategoryDao()
+
+  private val clickedCategories: MutableSet<Category> = mutableSetOf()
+  private val observableOfCategories = PublishSubject.create<Set<Category>>()
 
   private val subscriptions = CompositeDisposable()
   private val notes: MutableLiveData<Resource<List<Note>>> = MutableLiveData()
   private val categories: MutableLiveData<Resource<List<Category>>> = MutableLiveData()
 
   init {
-    val subscribeNotes = notesDao.all()
-      .doOnSubscribe { notes.value = Resource.Loading() }
+    val subscribeNotes = observableOfCategories
+      .doOnSubscribe { categories.value = Resource.Loading() }
+      .flatMap { noteCategoryDao.notesForCategories(it.map { cat -> cat.id }) }
+      .flatMap {
+        if (clickedCategories.isEmpty())
+          notesDao.all()
+        else
+          Observable.just(it)
+      }
       .uiObserve()
       .subscribe(
         { notes.value = Resource.Success(it) },
         { notes.value = Resource.Error(it) }
       )
+    observableOfCategories.onNext(clickedCategories)
 
     val subscribeCategories = categoriesDao.all()
       .doOnSubscribe { categories.value = Resource.Loading() }
@@ -38,6 +52,13 @@ class NotesViewModel : ViewModel() {
     subscriptions.addAll(subscribeNotes, subscribeCategories)
   }
 
+  fun onCategoryClicked(category: Category) {
+    if (category in clickedCategories)
+      clickedCategories -= category
+    else
+      clickedCategories += category
+    observableOfCategories.onNext(clickedCategories)
+  }
 
   fun allNotes(): LiveData<Resource<List<Note>>> = notes
 
