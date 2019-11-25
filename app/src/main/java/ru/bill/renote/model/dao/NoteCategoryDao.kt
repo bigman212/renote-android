dbf6c00aa31d8079d35135c3259acdcd5372f472
bigman212 <bigman212@yandex.ru>
2019-11-25 11:50:19 +1000

    (dev) add new query for NoteCategoryJoin table + refactor

--- app/src/main/java/ru/bill/renote/model/dao/NoteCategoryDao.kt
+++ app/src/main/java/ru/bill/renote/model/dao/NoteCategoryDao.kt
@@ -3,7 +3,8 @@ package ru.bill.renote.model.dao
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
-import io.reactivex.Single
+import io.reactivex.Maybe
+import io.reactivex.Observable
 import ru.bill.renote.model.entities.Category
 import ru.bill.renote.model.entities.Note
 import ru.bill.renote.model.entities.NoteCategoryJoin
@@ -11,26 +12,37 @@ import ru.bill.renote.model.entities.NoteCategoryJoin
 @Dao
 interface NoteCategoryDao {
   @Insert
-  fun insert(playlistSongJoin: NoteCategoryJoin)
+  fun insert(playlistSongJoin: NoteCategoryJoin): Maybe<Long>
 
   @Query(
     """
-           SELECT * FROM notes
+           SELECT DISTINCT id, title, body, source_link FROM notes
            INNER JOIN note_category_join
            ON notes.id=note_category_join.noteId
            WHERE note_category_join.categoryId=:categoryId
            """
   )
-  fun notesForCategory(categoryId: Long): Single<List<Note>>
+  // using observable to return [] instead of empty maybe
+  fun notesForCategory(categoryId: Long): Observable<List<Note>>
 
   @Query(
     """
-           SELECT * FROM categories
+           SELECT DISTINCT id, title, body, source_link FROM notes
+           INNER JOIN note_category_join
+           ON notes.id=note_category_join.noteId
+           WHERE note_category_join.categoryId IN (:categoriesId)
+           """
+  )
+  fun notesForCategories(categoriesId: List<Long>): Observable<List<Note>>
+
+  @Query(
+    """
+           SELECT DISTINCT id, name FROM categories
            INNER JOIN note_category_join
            ON categories.id=note_category_join.categoryId
            WHERE note_category_join.noteId=:noteId
            """
   )
-  fun categoriesForNote(noteId: Long): Single<List<Category>>
+  fun categoriesForNote(noteId: Long): Observable<List<Category>>
 
 }
