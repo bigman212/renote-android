d2e760b3f11300b769f8e5c9e586da4ede924de0
bigman212 <bigman212@yandex.ru>
2019-11-27 18:55:22 +1000

    (design) design note list
    - fix paddings for every element on screen
    - style note title and body
    - every second item in note list is gray

--- app/src/main/java/ru/bill/renote/notes/list/NotesListRVAdapter.kt
+++ app/src/main/java/ru/bill/renote/notes/list/NotesListRVAdapter.kt
@@ -3,13 +3,13 @@ package ru.bill.renote.notes.list
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.res.ResourcesCompat
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.rv_notes_item.view.*
 import ru.bill.renote.R
 import ru.bill.renote.model.entities.Note
 
-class NotesListRVAdapter(private var notesList: MutableList<Note> = mutableListOf())
-  : RecyclerView.Adapter<NotesListRVAdapter.ViewHolder>() {
+class NotesListRVAdapter(private var notesList: MutableList<Note> = mutableListOf()) : RecyclerView.Adapter<NotesListRVAdapter.ViewHolder>() {
 
   override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
     val inflater = LayoutInflater.from(parent.context)
@@ -22,7 +22,7 @@ class NotesListRVAdapter(private var notesList: MutableList<Note> = mutableListO
   }
 
   override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-    holder.bind(notesList[position])
+    holder.bind(notesList[position], position)
   }
 
   fun addAll(notesList: List<Note>) {
@@ -31,10 +31,14 @@ class NotesListRVAdapter(private var notesList: MutableList<Note> = mutableListO
     notifyDataSetChanged()
   }
 
-
   class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-    fun bind(note: Note) {
+    fun bind(note: Note, position: Int) {
       itemView.tv_note_title.text = note.title
+
+      if (position.rem(2) != 0) {
+        val grey = ResourcesCompat.getColor(itemView.resources, R.color.colorEven, null)
+        itemView.setBackgroundColor(grey)
+      }
     }
   }
 
